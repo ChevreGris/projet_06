28f86d09b0af0d379583c87e2417de9ab1a34239
mark.d <dumet.mark@gmail.com>
2022-01-21 04:52:07 +0100

    projet_06 v0.01.00 (page next et page back fonctionnent.)

--- main.js
+++ main.js
@@ -1,44 +1,129 @@
+var top_film_page = 1
+var adventure_film_page = 1
+var comedy_film_page = 1
+var musical_film_page = 1
 
-const img_top_films = document.getElementById("img_top_films");
-fetch('http://127.0.0.1:8000/api/v1/titles/?page_size=4')
-  .then(response => response.json())
-  .then(data => {
-    data.results.forEach((titles) => {
-      let img_src = document.createElement('img');
-      img_src.src = titles.image_url;
-      img_top_films.appendChild(img_src)
-    })
-  });
+function get_films(source, page, category) {
+  const films = document.getElementById(source);
+  fetch('http://127.0.0.1:8000/api/v1/titles/?page=' + page + '&page_size=4&genre=' + category + '&sort_by=-imdb_score')
+    .then(response => response.json())
+    .then(data => {
+      data.results.forEach((titles) => {
+        let div = document.createElement('div');
+        div.className = "container"
+        let img_src = document.createElement('img');
+        img_src.className = "raw_image"
+        img_src.src = titles.image_url;
+        div.appendChild(img_src)
+        let img_name = document.createElement('span');
+        img_name.className = "title"
+        img_name.textContent = titles.title;
+        div.appendChild(img_name)
+        films.appendChild(div)
+      })
+    });
+}
 
-const img_adventure = document.getElementById("img_adventure");
-fetch('http://127.0.0.1:8000/api/v1/titles/?page_size=4&genre=adventure')
-  .then(response => response.json())
-  .then(data => {
-    data.results.forEach((titles) => {
-      let img_src = document.createElement('img');
-      img_src.src = titles.image_url;
-      img_adventure.appendChild(img_src)
-    })
-  });
+function next(source, page, category) {
+  var parent = document.getElementById(source);
+  var child = parent.getElementsByClassName("container")[0];
+  parent.removeChild(child)
+  var child = parent.getElementsByClassName("container")[0];
+  parent.removeChild(child)
+  var child = parent.getElementsByClassName("container")[0];
+  parent.removeChild(child)
+  var child = parent.getElementsByClassName("container")[0];
+  parent.removeChild(child)
+  get_films(source, page, category)
+}
 
-const img_comedy = document.getElementById("img_comedy");
-fetch('http://127.0.0.1:8000/api/v1/titles/?page_size=4&genre=comedy')
-  .then(response => response.json())
-  .then(data => {
-    data.results.forEach((titles) => {
-      let img_src = document.createElement('img');
-      img_src.src = titles.image_url;
-      img_comedy.appendChild(img_src)
-    })
-  });
-  
-const img_musical = document.getElementById("img_musical");
-fetch('http://127.0.0.1:8000/api/v1/titles/?page_size=4&genre=musical')
-  .then(response => response.json())
-  .then(data => {
-    data.results.forEach((titles) => {
-      let img_src = document.createElement('img');
-      img_src.src = titles.image_url;
-      img_musical.appendChild(img_src)
-    })
-  });
+function back(source, page, category) {
+  var parent = document.getElementById(source);
+  var child = parent.getElementsByClassName("container")[0];
+  parent.removeChild(child)
+  var child = parent.getElementsByClassName("container")[0];
+  parent.removeChild(child)
+  var child = parent.getElementsByClassName("container")[0];
+  parent.removeChild(child)
+  var child = parent.getElementsByClassName("container")[0];
+  parent.removeChild(child)
+ get_films(source, page, category)
+}
+
+
+get_films("img_top_films", top_film_page, "")
+get_films("img_adventure", adventure_film_page, "adventure")
+get_films("img_comedy", comedy_film_page, "comedy")
+get_films("img_musical", musical_film_page, "musical")
+
+function top_films_next() {
+  top_film_page = top_film_page + 1;
+  next("img_top_films", top_film_page, "")
+}
+
+function top_films_back() {
+  if (top_film_page == 1) {
+    top_film_page == 1;
+  }
+  if (top_film_page > 1) {
+    top_film_page = top_film_page - 1;
+    back("img_top_films", top_film_page, "")
+  }
+}
+
+function adventure_next() {
+  adventure_film_page = adventure_film_page + 1;
+  next("img_adventure", adventure_film_page, "adventure")
+}
+
+function adventure_back() {
+  if (adventure_film_page == 1) {
+    adventure_film_page == 1;
+  }
+  if (adventure_film_page > 1) {
+    adventure_film_page = adventure_film_page - 1;
+    back("img_adventure", adventure_film_page, "adventure")
+  }
+}
+
+function comedy_next() {
+  comedy_film_page = comedy_film_page + 1;
+  next("img_comedy", comedy_film_page, "comedy")
+}
+
+function comedy_back() {
+  if (comedy_film_page == 1) {
+    comedy_film_page == 1;
+  }
+  if (comedy_film_page > 1) {
+    comedy_film_page = comedy_film_page - 1;
+    back("img_comedy", comedy_film_page, "comedy")
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function musical_next() {
+  musical_film_page = musical_film_page + 1;
+  next("img_musical", musical_film_page, "musical")
+}
+
+function musical_back() {
+  if (musical_film_page == 1) {
+    musical_film_page == 1;
+  }
+  if (musical_film_page > 1) {
+    musical_film_page = musical_film_page - 1;
+    back("img_musical", musical_film_page, "musical")
+  }
+}
